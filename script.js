0e54aab9f710bfd96b7e9befb4d126cb593f058a
Ronaldo <ronaldovctor@outlook.com>
2022-09-26 15:23:47 -0300

    Section animations

--- script.js
+++ script.js
@@ -30,7 +30,50 @@ function initAccordion() {
 	}
 }
 
+function initScroolToSection() {
+	const internalLinks = document.querySelectorAll('.js-menu a[href^="#"]')
+
+	function scrollToSection(event) {
+		event.preventDefaul()
+		const href = event.currentTarget.getAttribute('href')
+		const section = document.querySelector(href)
+
+		// window.scrollTo({
+		// 	top: section.offsetTop.accordionList,
+		// 	behavior: 'smooth',
+		// })
+
+		// alternative
+		section.scrollinToView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	}
+
+	internalLinks.forEach((link) => {
+		link.addEventListener('click', scrollToSection)
+	})
+}
+
+function initScrollAnimation() {
+	const sections = document.querySelectorAll('.js-scroll')
+
+	function animaScroll(element) {
+		element.classList.add('active')
+	}
+
+	sections.forEach((section) => {
+		const windowHeight = window.innerHeight * 0.65
+		const sectionTop = section.getBoundingClientRect().top
+		sectionTop < windowHeight && animaScroll(section)
+	})
+}
+
 window.addEventListener('load', () => {
 	initTabNav()
 	initAccordion()
+	initScroolToSection()
+	initScrollAnimation()
 })
+
+window.addEventListener('scroll', initScrollAnimation)
