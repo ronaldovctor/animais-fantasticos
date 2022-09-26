b74b6f4c9954f44530a680999a7bcdfd31d6d32e
Ronaldo <ronaldovctor@outlook.com>
2022-09-26 12:54:19 -0300

    Add accordion list

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
+function initTabNav() {
+	const tabMenu = document.querySelectorAll('.js-tabmenu li')
+	const tabContent = document.querySelectorAll('.js-tabcontent section')
 
-function activeTab(index) {
-	cleanTab()
-	tabContent[index].classList.add('active')
-}
+	function activeTab(index) {
+		cleanTab()
+		tabContent[index].classList.add('active')
+	}
 
-function cleanTab() {
-	tabContent.forEach((section) => {
-		section.classList.remove('active')
+	function cleanTab() {
+		tabContent.forEach((section) => {
+			section.classList.remove('active')
+		})
+	}
+
+	tabMenu.forEach((item, index) => {
+		item.addEventListener('click', () => activeTab(index))
 	})
 }
 
-tabMenu.forEach((item, index) => {
-	item.addEventListener('click', () => activeTab(index))
+function initAccordion() {
+	const accordionList = document.querySelectorAll('.js-accordion dt')
+	accordionList.forEach((item) => {
+		item.addEventListener('click', activeAccordion)
+	})
+
+	function activeAccordion() {
+		this.classList.toggle('active')
+		this.nextElementSibling.classList.toggle('active')
+	}
+}
+
+window.addEventListener('load', () => {
+	initTabNav()
+	initAccordion()
 })
