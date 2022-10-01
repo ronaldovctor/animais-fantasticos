38ab049d5f993155301b3e2b317784f9eae1ec0e
Ronaldo <ronaldovctor@outlook.com>
2022-10-01 19:52:31 -0300

    Adding login modal

---
+++ modules/modal.js
@@ -0,0 +1,18 @@
+export function initModal() {
+	const botaoAbrir = document.querySelector('[data-modal="open"]')
+	const botaoFechar = document.querySelector('[data-modal="close"]')
+	const containerModal = document.querySelector('[data-modal="container"]')
+
+	botaoAbrir.addEventListener('click', toggleModal)
+	botaoFechar.addEventListener('click', toggleModal)
+	containerModal.addEventListener('click', clickOutModal)
+
+	function toggleModal(event) {
+		event.preventDefault()
+		containerModal.classList.toggle('active')
+	}
+
+	function clickOutModal(event) {
+		event.target == this && toggleModal(event)
+	}
+}
--- script.js
+++ script.js
@@ -2,12 +2,14 @@ import { initTabNav } from './modules/tab-nav.js'
 import { initAccordion } from './modules/accordion.js'
 import { initScroolToSection } from './modules/scroll-to-section.js'
 import { initScrollAnimation } from './modules/scroll-animation.js'
+import { initModal } from './modules/modal.js'
 
 window.addEventListener('load', () => {
 	initTabNav()
 	initAccordion()
 	initScroolToSection()
 	initScrollAnimation()
+	initModal()
 })
 
 window.addEventListener('scroll', initScrollAnimation)
