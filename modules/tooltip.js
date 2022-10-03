7d304772372b603041074a9f295cf32d654d35ea
Ronaldo <ronaldovctor@outlook.com>
2022-10-03 11:07:53 -0300

    Dropdown menu in navbar verifying the click target

---
+++ modules/tooltip.js
@@ -0,0 +1,48 @@
+export function initTooltip() {
+	const tooltips = document.querySelectorAll('[data-tooltip]')
+
+	tooltips.forEach((item) => {
+		item.addEventListener('mouseover', onMouseOver)
+	})
+
+	function onMouseOver(event) {
+		const tooltipBox = createToolTipBox(this)
+		tooltipBox.style.top = `${event.pageY}px`
+		tooltipBox.style.left = `${event.pageX}px`
+
+		onMouseLeave.tooltipBox = tooltipBox
+		this.addEventListener('mousemove', onMouseMove)
+
+		onMouseMove.tooltipBox = tooltipBox
+		onMouseLeave.element = this
+		this.addEventListener('mouseleave', onMouseLeave)
+	}
+
+	const onMouseLeave = {
+		handleEvent() {
+			this.tooltipBox.remove()
+			this.element.removeEventListener('mouseleave', onMouseLeave)
+			this.element.removeEventListener('mousemove', onMouseMove)
+		},
+	}
+
+	const onMouseMove = {
+		handleEvent(event) {
+			// function called handleEvent inside object runs as function to an eventListener like a normal function
+			this.tooltipBox.style.top = `${event.pageY + 20}px` // add 20px on top to make tooltip not be under the mouse and run the mouseLeave function
+			this.tooltipBox.style.left = `${event.pageX}px`
+		},
+	}
+
+	function createToolTipBox(element) {
+		const tooltipBox = document.createElement('div')
+		const text = element.getAttribute('aria-label')
+
+		tooltipBox.classList.add('tooltip')
+		tooltipBox.innerText = text
+
+		document.body.appendChild(tooltipBox)
+
+		return tooltipBox
+	}
+}
--- script.js
+++ script.js
@@ -3,6 +3,8 @@ import { initAccordion } from './modules/accordion.js'
 import { initScroolToSection } from './modules/scroll-to-section.js'
 import { initScrollAnimation } from './modules/scroll-animation.js'
 import { initModal } from './modules/modal.js'
+import { initTooltip } from './modules/tooltip.js'
+import { initDropdownMenu } from './modules/dropdown-menu.js'
 
 window.addEventListener('load', () => {
 	initTabNav()
@@ -10,6 +12,8 @@ window.addEventListener('load', () => {
 	initScroolToSection()
 	initScrollAnimation()
 	initModal()
+	initTooltip()
+	initDropdownMenu()
 })
 
 window.addEventListener('scroll', initScrollAnimation)
