bfbe956a6ca93d32012b5287166560b5e6c1d9fe
Ronaldo <ronaldovctor@outlook.com>
2022-10-10 14:22:35 -0300

    funcionamento refactoring complete

--- modules/funcionamento.js
+++ modules/funcionamento.js
@@ -1,15 +1,38 @@
-export default function initFuncionamento() {
-	const funcionamento = document.querySelector('[data-semana]')
-	const diasSemana = funcionamento.dataset.semana.split(',').map(Number)
-	const horarioSemana = funcionamento.dataset.horario.split(',').map(Number)
+export default class Funcionamento {
+	constructor(funcionamento) {
+		this.funcionamento = document.querySelector(funcionamento)
+	}
 
-	const dataAgora = new Date()
-	const diaAgora = dataAgora.getDay()
-	const horarioAgora = dataAgora.getHours()
+	dadosFuncionamento() {
+		this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number)
+		this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number)
+	}
 
-	const semanaAberto = diasSemana.indexOf(diaAgora) !== -1
-	const horarioAberto =
-		horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]
+	dadosAgora() {
+		this.dataAgora = new Date()
+		this.diaAgora = this.dataAgora.getDay()
+		this.horarioAgora = this.dataAgora.getUTCHours() - 3
+	}
 
-	if (semanaAberto && horarioAberto) funcionamento.classList.add('open')
+	isAberto() {
+		this.semanaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1
+		this.horarioAberto =
+			this.horarioAgora >= this.horarioSemana[0] &&
+			this.horarioAgora < this.horarioSemana[1]
+
+		return this.semanaAberto && this.horarioAberto
+	}
+
+	activeAberto() {
+		this.isAberto() && this.funcionamento.classList.add('open')
+	}
+
+	init() {
+		if (this.funcionamento) {
+			this.dadosFuncionamento()
+			this.dadosAgora()
+			this.activeAberto()
+		}
+		return this
+	}
 }
